7e020bb745134f346458681f9d2b8d04ae7345bf
Duy Pham Le <phamleduy04@gmail.com>
2022-01-17 16:51:28 -0600

    change http client to undici + fix symbol not found

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-const axios = require('axios');
 const cheerio = require('cheerio').default;
 const { dateToUnix, handleError, handleResponse } = require('./util');
 const frequencyList = ['1d', '1wk', '1mo'];
 var numeral = require('numeral');
-const numeralConfig = require('./numeralConfig.json');
-numeral.register('locale', 'dui', numeralConfig); // dui is my nickname, just for custom config xD
+numeral.register('locale', 'dui', require('./numeralConfig.json')); // dui is my nickname, just for custom config xD
 numeral.locale('dui');
-const instance = axios.create({
-    baseURL: 'https://finance.yahoo.com/quote/',
-    timeout: 5000
-});
+const { request } = require('undici');
+const baseURL = 'https://finance.yahoo.com/quote/';
 const camelCase = require('camelcase');
 
 const mapRows = (_, row) => {
@@ -26,7 +22,7 @@ const mapRows = (_, row) => {
 
 
 module.exports = {
-    getHistoricalPrices: async function(startDate, endDate, symbol, frequency) {
+    getHistoricalPrices: async (startDate, endDate, symbol, frequency) => {
         try {
             if (!startDate || Object.prototype.toString.call(startDate) !== '[object Date]') throw new Error('startDate not provided or not a "Date" type!');
             if (!endDate || Object.prototype.toString.call(startDate) !== '[object Date]') throw new Error('endDate not provided or not a "Date" type!');
@@ -34,22 +30,22 @@ module.exports = {
             if (!frequency || typeof frequency !== 'string' || !frequencyList.includes(frequency)) throw new Error('Frequency should be "1d", "1wk" or "1mo"');
             const period1 = dateToUnix(startDate);
             const period2 = dateToUnix(endDate) + 86400; // plus 1 day
-            const res = await instance.get(`/${symbol}/history?period1=${period1}&period2=${period2}&interval=${frequency}&filter=history&frequency=${frequency}`);
-            if (res.request._redirectable._redirectCount !== 0) throw new Error('Yahoo Finance is redirected, maybe your symbol is not found!');
-            const $ = cheerio.load(res.data);
+            const { body } = await request(baseURL + `/${symbol}/history?period1=${period1}&period2=${period2}&interval=${frequency}&filter=history&frequency=${frequency}`);
+            const responseBody = await body.text();
+            const $ = cheerio.load(responseBody);
+            if ($('title').text() == 'Requested symbol wasn\'t found') throw new Error('Symbol not found!');
             const currency = $('#Col1-1-HistoricalDataTable-Proxy > section > div > div > span > span').text().replace('Currency in', '').trim();
-            return handleResponse(JSON.parse(res.data.split('HistoricalPriceStore":{"prices":')[1].split(",\"isPending")[0]), currency);
+            return handleResponse(JSON.parse(responseBody.split('HistoricalPriceStore":{"prices":')[1].split(",\"isPending")[0]), currency);
         }
         catch(err) {
             return handleError(err);
         }
     },
-    getSymbol: async function(symbol) {
+    getSymbol: async (symbol) => {
         try {
             if (!symbol || typeof symbol !== 'string') throw new Error('Symbol not provided or Symbol is not a string!');
-            const res = await instance.get(`${symbol}/`);
-            if (res.request._redirectable._redirectCount !== 0) throw new Error('Yahoo Finance is redirected, maybe your symbol is not found!');
-            const $ = cheerio.load(res.data);
+            const { body } = await request(baseURL + `${symbol}/`);
+            const $ = cheerio.load(await body.text());
             let currency = $('#quote-header-info > div.Mt\\(15px\\) > div > div > span').text()
             currency = currency ? currency.split(' ').pop() : undefined;
             const col1 = $('#quote-summary > div.Pend\\(12px\\) > table > tbody').map(mapRows).get()[0];
--- test/test.js
+++ test/test.js
@@ -1,9 +1,7 @@
 const yahooStockAPI = require('../index.js');
-async function main() {
+( async() => {
     console.log(await yahooStockAPI.getSymbol('AAPL'));
     const startDate = new Date('08/21/2020');
     const endDate = new Date('08/26/2020');
     console.log(await yahooStockAPI.getHistoricalPrices(startDate, endDate, 'AAPL', '1d'));
-}
-
-main();
+})();
